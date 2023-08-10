a2832bb30536740c6a2710ac6be0dd4b6e4000f5
stephen-fraser <steve.james.fraser@gmail.com>
2023-08-10 12:58:55 -0600

    module 4 initial commit

---
+++ tinkering.js
@@ -0,0 +1,10 @@
+const age = 22
+
+if (age < 13) {
+  console.log('Elementary School')
+} else if (age >= 13 && age <= 18) {
+  console.log ('Secondary School')
+} else {
+  console.log ('Lighthouse Labs')
+}
+
